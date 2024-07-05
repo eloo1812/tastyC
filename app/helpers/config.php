6d58311cb7032cbcf940290a4f4e7ef8b4380469
eloo1812 <142434356+eloo1812@users.noreply.github.com>
2024-07-05 08:01:10 -0300

    Delete app directory

--- app/helpers/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace app\helpers;
-
-define('ROOT', dirname(__FILE__, 3));
-define('DIR_VIEWS', ROOT . '/app/views/');
-define('EXT_VIEWS', '.html');
--- app/routes/site.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace app\routes; //cntrl alt g
-
-use app\controllers\Entrar;
-
-$app->get('/cadastro', Entrar::class . ":create");
-
